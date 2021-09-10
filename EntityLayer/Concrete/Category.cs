a7a2d6d6c0d7a570453c69f5d49228959ff0e539
Muhammet Şirin <muhammetsirin07@gmail.com>
2021-09-10 17:48:14 +0300

    10.09.2021 Ders Sonu

--- EntityLayer/Concrete/Category.cs
+++ EntityLayer/Concrete/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace EntityLayer.Concrete
     public class Category
     {
         // Erişim Belirleyici Türü -Değişken Türü - İsim - {get set}
+        [Key]
         public int CategoryID { get; set; }
         public string CategoryName { get; set; }
         public string CategoryDescription { get; set; }
